d02f0cd9b006694c78dbaf00b8f4057a4c5e0ddd
Hengsheng-Liu <lhs7714@bu.edu>
2024-10-09 20:56:27 -0400

    complete until task2

--- mini_fb/models.py
+++ mini_fb/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.utils import timezone
 class Profile(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
@@ -8,3 +8,12 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
+    def get_status_messages(self):
+        return self.statusmessage_set.all().order_by('-timestamp')
+class StatusMessage(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    message = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.profile} : {self.message} - {self.timestamp}'
--- mini_fb/views.py
+++ mini_fb/views.py
@@ -1,7 +1,12 @@
 from django.views.generic import ListView
+from django.views.generic.detail import DetailView
 from .models import Profile
 
 class ShowAllProfilesView(ListView):
     model = Profile
     template_name = 'mini_fb/show_all_profiles.html'
     context_object_name = 'profiles'
+class ShowProfilePageView(DetailView):
+    model = Profile
+    template_name = 'mini_fb/show_profile_page.html'
+    context_object_name = 'profile'

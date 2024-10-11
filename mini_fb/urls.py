ba9327a1e06648ab94efed05cc6d06dee97013e2
Hengsheng-Liu <lhs7714@bu.edu>
2024-10-10 21:37:29 -0400

    complete the assignment

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import CreateProfileView, ShowAllProfilesView, ShowProfilePageView
+from .views import CreateProfileView, CreateStatusMessageView, ShowAllProfilesView, ShowProfilePageView
 
 urlpatterns = [
     path('', ShowAllProfilesView.as_view(), name='show_all_profiles'),
     path('profile/<int:pk>/', ShowProfilePageView.as_view(), name='show_profile'),
-    path('create_profile/', CreateProfileView.as_view(), name='create_profile')
+    path('create_profile/', CreateProfileView.as_view(), name='create_profile'),
+    path('profile/<int:pk>/create_status', CreateStatusMessageView.as_view(), name='create_status')
 ]
--- mini_fb/views.py
+++ mini_fb/views.py
@@ -3,8 +3,8 @@ from django.urls import reverse
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
 
-from .models import Profile
-from .forms import CreateProfileForm
+from .models import Profile, StatusMessage
+from .forms import CreateProfileForm, CreateStatusMessageForm
 class ShowAllProfilesView(ListView):
     model = Profile
     template_name = 'mini_fb/show_all_profiles.html'
@@ -17,5 +17,23 @@ class CreateProfileView(CreateView):
     model = Profile
     form_class = CreateProfileForm
     template_name = 'mini_fb/create_profile_form.html'
+class CreateStatusMessageView(CreateView):
+    model = StatusMessage
+    form_class = CreateStatusMessageForm
+    template_name = 'mini_fb/create_status_form.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['profile'] = Profile.objects.get(pk=self.kwargs['pk'])
+        return context
+
+    def form_valid(self, form):
+        profile = Profile.objects.get(pk=self.kwargs['pk'])
+        form.instance.profile = profile
+        return super().form_valid(form)
+    
+    def get_success_url(self):
+        # Redirect back to the profile page
+        return reverse('show_profile', kwargs={'pk': self.kwargs['pk']})
 
     
